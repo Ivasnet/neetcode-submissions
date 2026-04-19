cfa954040d0cac0b93caacc809f7d0e9cae4be1e
Ivan Snetkov <70843270+Ivasnet@users.noreply.github.com>
2026-04-19 10:58:51 +0300

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] is '(' or '{' or '[')
+            {
+                stack.Push(s[i]);
+            }
+            else
+            {
+                if (stack.Count() == 0) return false;
+                
+                var last = stack.Pop();
+                if ((s[i] == ')' && last == '(')
+                || (s[i] == '}' && last == '{')
+                || (s[i] == ']' && last == '['))
+                {
+                    continue;
+                }
+
+                return false;
+            }
+        }
+
+        if (stack.Count() == 0) return true;
+
+        return false;
+    }
+}
